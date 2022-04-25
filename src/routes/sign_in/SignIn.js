c2d3ca11b2ff54b502caa29d05df8a321bc32da4
w3tsa <tanveersayem@gmail.com>
2022-04-25 01:25:25 -0400

    initiated firebase auth

--- src/routes/sign_in/SignIn.js
+++ src/routes/sign_in/SignIn.js
@@ -1,5 +1,16 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase";
+
 const SignIn = () => {
-  return <div>signIn</div>;
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
+  return (
+    <div>
+      <h1>Sign in</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </div>
+  );
 };
 
 export default SignIn;
