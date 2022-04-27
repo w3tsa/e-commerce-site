87cf0752c9710c139aa3f07fce550f96d66391f6
w3tsa <tanveersayem@gmail.com>
2022-04-26 22:08:25 -0400

    :tada: added firebase config .env files and initialize snapshot from the db

--- src/routes/sign_in/SignIn.js
+++ src/routes/sign_in/SignIn.js
@@ -1,9 +1,12 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
+    const { user } = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user);
   };
   return (
     <div>
--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -6,21 +6,36 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
+  projectId: process.env.REACT_APP_PROJECT_Id,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
 };
-const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth();
+const app = initializeApp(firebaseConfig);
+
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
   prompt: "select_account",
 });
+
+const auth = getAuth();
 const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
-export { auth, signInWithGooglePopup };
+const db = getFirestore();
+
+const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+};
+
+export { auth, signInWithGooglePopup, createUserDocumentFromAuth };
