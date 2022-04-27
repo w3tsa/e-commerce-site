215e4aa4a338e60584cff82d8bfe551a6f07dc3f
w3tsa <tanveersayem@gmail.com>
2022-04-26 22:22:06 -0400

    :rainbows: added our sign in component as well as firebase utils and brought in firebase library

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -30,12 +30,25 @@ const db = getFirestore();
 
 const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, "users", userAuth.uid);
-
-  console.log(userDocRef);
-
   const userSnapshot = await getDoc(userDocRef);
-  console.log(userSnapshot);
-  console.log(userSnapshot.exists());
+
+  // if user data does not exist
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+  // if it does, return user data
+  return userDocRef;
 };
 
 export { auth, signInWithGooglePopup, createUserDocumentFromAuth };
