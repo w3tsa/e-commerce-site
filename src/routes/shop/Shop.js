dc2f77c06bd7ebdded949bb6d82f0976447ae03c
w3tsa <tanveersayem@gmail.com>
2022-05-31 14:34:23 -0400

    :tada: shop page is done with product card

--- src/routes/shop/Shop.js
+++ src/routes/shop/Shop.js
@@ -2,17 +2,17 @@ import { useContext } from "react";
 
 // import context
 import { ProductsContext } from "../../contexts/products";
+import ProductCard from "../../components/product_card/ProductCard";
+
+// styles
+import "./Shop.scss";
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
   return (
-    <div>
-      {products.map(({ id, name, imageUrl, price }) => {
-        return (
-          <div key={id}>
-            <h1>{name}</h1>
-          </div>
-        );
+    <div className="products-container">
+      {products.map((product) => {
+        return <ProductCard key={product.id} product={product} />;
       })}
     </div>
   );
