f34ce213391303aac958798419108bef25477a30
w3tsa <tanveersayem@gmail.com>
2022-06-02 21:30:25 -0400

    updated checkout

---
+++ src/routes/checkout/Checkout.js
@@ -0,0 +1,41 @@
+import "./Checkout.scss";
+
+import { useContext } from "react";
+
+// context
+import { CartContext } from "../../contexts/cart";
+
+// local import
+import CheckoutItem from "../../components/checkout_item/CheckoutItem";
+
+const Checkout = () => {
+  const { cartItems, cartTotal } = useContext(CartContext);
+
+  return (
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map((item) => (
+        <CheckoutItem key={item.id} cartItem={item} />
+      ))}
+      <span className="total">Total: ${cartTotal}</span>
+    </div>
+  );
+};
+
+export default Checkout;
